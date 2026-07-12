ab850a983cc8171f3aef1b6f32ec05afd6b4f134
Adistian Herlambang <adistian59@gmail.com>
2026-07-13 06:54:27 +0700

    up

--- api/index.php
+++ api/index.php
@@ -15,5 +15,5 @@ putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/services.php');
 
-// Forward Vercel requests to normal index.php
+// Forward Vercel requests to normal index.ph
 require __DIR__ . '/../public/index.php';
